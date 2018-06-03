f6a7fdd8cf01ed188387d6ca18b8e495e95a42d8
JuanD1 <juandacor@outlook.com>
2018-06-02 21:34:10 -0500

    Led controlado por interface grafica

--- src/controllers/led.js
+++ src/controllers/led.js
@@ -1,18 +1,47 @@
-//controller on led
-exports.index = (req, res) => {	
-	res.render('index', {
-		title:'arduino y johnny five'
-	})
-}
+//Dependencie
+let five = require('johnny-five') //llamando la libreria
 
-exports.onLed = (req, res) => {	
-	res.render('index', {
-		title:'arduino y johnny five'
-	})
-}
+var myBoard = new five.Board() //instanciando la placa (Arduino UNO) automaticamente {port: '\\\\.\\COM4' }
+var devices = {}
 
-exports.offLed = (req, res) => {	
-	res.render('index', {		
-		title:'arduino y johnny five'
+myBoard.on('ready', function (){ //constructor		
+	devices.led = new five.Led(13) //instanciando el pin del led
+	let myLed = devices.led
+	console.log('Board is ready')
+
+	this.repl.inject({ // permite el acceso mientras el programa esta corriendo
+		myLed: myLed
 	})	
-}
+
+	// this.on('exit', ()=>{ //se ejecuta antes de que el programa se "desconecte" de la placa
+	// 	myLed.off()
+	// })
+})
+
+myBoard.on('error', (error)=>{ //captura le error
+	console.log(error)
+})
+
+	//controller on led
+	exports.index = (req, res) => {	
+		console.log('en index')
+		res.render('index', {
+			title:'arduino y johnny five'
+		})
+	}
+	
+	exports.onLed = (req, res) => {
+		console.log('Led on')
+		myBoard.isReady ? devices.led.on() : console.log("Board isn't conected")
+		res.render('index', {
+			title:'arduino y johnny five'
+		})
+	}
+	
+	exports.offLed = (req, res) => {
+		console.log('Led off')
+		myBoard.isReady ? devices.led.off() : console.log("Board isn't conected")	
+		res.render('index', {
+			title:'arduino y johnny five'
+		})	
+	}
