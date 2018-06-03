1b54cb18b3c725d78e693fef9812d992df4d1d32
JuanD1 <juandacor@outlook.com>
2018-06-02 19:44:56 -0500

    creando vistas y estructura

--- led.js
+++ led.js
@@ -1,16 +1,20 @@
-let five = require('johnny-five')
-// const {Board, Led} = require('johnny-five')
+let five = require('johnny-five') 							//llamando la libreria
+// const {Board, Led} = require('johnny-five')	//llamando la libreria
 
-let myBoard = new five.Board()
+let myBoard = new five.Board() //instanciando la placa (Arduino UNO) automaticamente
 
-myBoard.on('ready', function (){
-	let myLed = new five.Led(13)
-	// myLed.strobe()
-	this.repl.inject({
+myBoard.on('ready', function (){ //constructor
+	let myLed = new five.Led(13) //instanciando el pin del led
+
+	this.repl.inject({ // permite el acceso mientras el programa esta corriendo
 		myLed: myLed
 	})	
+
+	// this.on('exit', ()=>{ //se ejecuta antes de que el programa se "desconecte" de la placa
+	// 	myLed.off()
+	// })
 })
 
-myBoard.on('error', (error)=>{
+myBoard.on('error', (error)=>{ //captura le error
 	console.log(error)
 })
