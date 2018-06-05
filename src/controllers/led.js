41a39500f61ea1fe48cc91f10ef5a054a9064f23
JuanD1 <juandacor@outlook.com>
2018-06-04 19:36:29 -0500

    -

--- src/controllers/led.js
+++ src/controllers/led.js
@@ -22,29 +22,29 @@ myBoard.on('error', (error)=>{ //captura le error
 	console.log(error)
 })
 
-	//controller on led
-	exports.index = (req, res) => {	
-		console.log('en index')
-		res.render('index', {
-			title:'Arduino y johnny five',
-			comment: 'Turn on or off your led'
-		})
-	}
-	
-	exports.onLed = (req, res) => {
-		console.log('Led on')
-		myBoard.isReady ? devices.led.on() : console.log("Board isn't conected")
-		res.render('index', {
-			title: 'Arduino y johnny five',
-			comment: 'Led on'
-		})
-	}
-	
-	exports.offLed = (req, res) => {
-		console.log('Led off')
-		myBoard.isReady ? devices.led.off() : console.log("Board isn't conected")	
-		res.render('index', {
-			title:'Arduino y johnny five',
-			comment: 'Led off'
-		})	
-	}
+//controller on led
+exports.index = (req, res) => {	
+	console.log('en index')
+	res.render('index', {
+		title:'Arduino y johnny five',
+		comment: 'Turn on or off your led'
+	})
+}
+
+exports.onLed = (req, res) => {
+	console.log('Led on')
+	myBoard.isReady ? devices.led.on() : console.log("Board isn't conected")
+	res.render('index', {
+		title: 'Arduino y johnny five',
+		comment: 'Led on'
+	})
+}
+
+exports.offLed = (req, res) => {
+	console.log('Led off')
+	myBoard.isReady ? devices.led.off() : console.log("Board isn't conected")	
+	res.render('index', {
+		title:'Arduino y johnny five',
+		comment: 'Led off'
+	})	
+}
